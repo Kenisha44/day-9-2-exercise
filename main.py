b455524eb38182403f2d936029fbfc2c18b72ce6
Kenisha Johnson <kenishaajohnson@gmail.com>
2022-11-21 22:13:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,5 @@ def add_new_country(country_visited, times_visited, cities_visited):
 #🚨 Do not change the code below
 add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
 print(travel_log)
-#udemy 
 
 
